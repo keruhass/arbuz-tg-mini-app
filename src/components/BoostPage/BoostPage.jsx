396678d46f15138ce8940aa562a1f89d33631e57
keruhass <kirillka0423@gmail.com>
2024-01-24 22:24:53 +0700

    update-7

--- src/components/BoostPage/BoostPage.jsx
+++ src/components/BoostPage/BoostPage.jsx
@@ -13,9 +13,9 @@ const BoostPage = () => {
 
         <div className="boostpage">
 
-            <div><h1>Tap boost &#128070;</h1><br></br><h4>This means that you will receive more 'arbuz' per click</h4></div>
-            <div><h1>Energy boost &#9889;</h1><br></br><h4>This means you will have more energy to spend</h4></div>
-            
+            <div><h1>Tap boost &#128070;</h1><br></br><h5>This means that you will receive more 'arbuz' per click</h5></div>
+            <div><h1>Energy boost &#9889;</h1><br></br><h5>This means you will have more energy to spend</h5></div>
+
         </div>
     )
 
